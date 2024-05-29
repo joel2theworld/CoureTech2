290abd50c34ad48e5e220e8e6448f07ba0d8f76c
Joel Eriba <joel.eriba@decagon.dev>
2024-05-29 18:36:19 +0100

    Add files via upload

---
+++ PhoneNumbers.Infrastructure/Data/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumbers.Infrastructure.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+
+            if (context.Countries.Any())
+            {
+                return;   // Data was already seeded
+            }
+
+            context.SaveChanges();
+        }
+    }
+
+}
---
+++ PhoneNumbers.Infrastructure/Repository/CountryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneNumbers.Core.Entities;
+using PhoneNumbers.Core.IRepository;
+using PhoneNumbers.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumbers.Infrastructure.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CountryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Country GetCountryByCode(string countryCode)
+        {
+            return _context.Countries
+                .Include(c => c.CountryDetails)
+                .FirstOrDefault(c => c.CountryCode == countryCode);
+        }
+    }
+
+}
